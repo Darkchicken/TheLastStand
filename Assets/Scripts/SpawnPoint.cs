793393aecde66f860cdaa8be3f3aa7930fb63588
Nesskittycat <Nessa@dhcp-33-4-243.dhcpint.csulb.edu>
2015-03-08 13:50:40 -0700

    SpawnPointCode
    Added SpawnPoint Script

---
+++ Assets/Scripts/SpawnPoint.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPoint : MonoBehaviour {
+	
+	
+	public Transform Enemy;
+	private bool EnemyCheck = false;
+	private float Timer;
+	
+	void  Awake (){
+		Timer = Time.time + 10;
+	}
+	void  Start (){
+		Timer = Time.time + 10;
+	}
+	
+	void  Update (){
+		if (Timer < Time.time && !EnemyCheck) { // check if real time has caught up with timer
+			Instantiate(Enemy, transform.position, transform.rotation);
+			Timer = Time.time + 5;
+			EnemyCheck = true;
+		}
+	}
+}
